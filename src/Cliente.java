9a02d937f190ada9b7b2eee5a68e67810ee863bf
Jess <jesjimenez@estudiantec.cr>
2023-08-25 15:30:13 -0600

    Un cliente envia mensajes al servidor

--- src/Cliente.java
+++ src/Cliente.java
@@ -2,6 +2,12 @@
 
 import javax.swing.*;
 
+import java.awt.event.*;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+import java.net.*;
 
 public class Cliente {
 
@@ -35,28 +41,105 @@ class MarcoCliente extends JFrame{
 class LaminaMarcoCliente extends JPanel{
 	
 	public LaminaMarcoCliente(){
+
+		name= new JTextField(5);
+		add(name);
 	
-		JLabel texto=new JLabel("CLIENTE");
-		
+		JLabel texto=new JLabel(" CHAT CLIENTE");
 		add(texto);
+
+		ip= new JTextField(8);
+		add(ip);
+
+		campochat= new JTextArea(12,20) ;
+		add(campochat);
 	
 		campo1=new JTextField(20);
 	
 		add(campo1);		
 	
 		miboton=new JButton("Enviar");
+
+		EnviaTexto mievento= new EnviaTexto();
+		miboton.addActionListener(mievento);
 		
 		add(miboton);	
 		
 	}
 	
+	private class EnviaTexto implements ActionListener{
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			// TODO Auto-generated method stub
+			
+			//System.out.println ("Hola funciona");
+
+			try {
+				Socket misocket= new Socket("192.168.100.16", 5000);
+				
+				EnvioCompleto datos= new EnvioCompleto();
+				datos.setName(name.getText());
+				datos.setIp(ip.getText());
+				datos.setMensaje(campo1.getText());
+				ObjectOutputStream paquete_Datos=new ObjectOutputStream(misocket.getOutputStream());
+
+				paquete_Datos.writeObject(datos);
+				misocket.close();
+
+
+				/*DataOutputStream flujo_salida= new DataOutputStream(misocket.getOutputStream());
+
+				flujo_salida.writeUTF(campo1.getText());
+				flujo_salida.close(); */
+
+			} catch (UnknownHostException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			} catch (IOException e1) {
+				// TODO Auto-generated catch block
+				System.out.println (e1.getMessage());
+			} 
+		}
 	
-	
+	}
+
 		
-		
-		
-	private JTextField campo1;
+	private JTextField campo1, name, ip;
+
+	private JTextArea campochat;
 	
 	private JButton miboton;
 	
+}
+
+class EnvioCompleto implements Serializable{
+
+	private String name, ip, mensaje;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getIp() {
+		return ip;
+	}
+
+	public void setIp(String ip) {
+		this.ip = ip;
+	}
+
+	public String getMensaje() {
+		return mensaje;
+	}
+
+	public void setMensaje(String mensaje) {
+		this.mensaje = mensaje;
+	}
+
+
 }
--- src/Servidor.java
+++ src/Servidor.java
@@ -3,6 +3,8 @@
 import javax.swing.*;
 
 import java.awt.*;
+import java.io.*;
+import java.net.*;
 
 public class Servidor  {
 
@@ -16,7 +18,7 @@ public class Servidor  {
 	}	
 }
 
-class MarcoServidor extends JFrame {
+class MarcoServidor extends JFrame implements Runnable{
 	
 	public MarcoServidor(){
 		
@@ -33,8 +35,56 @@ class MarcoServidor extends JFrame {
 		add(milamina);
 		
 		setVisible(true);
+
+		Thread mihilo= new Thread(this);
+		mihilo.start();
+
 		
 		}
 	
+	
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		//System.out.println("Estoy a la escucha");
+		try {
+			ServerSocket servidor = new ServerSocket(5000);
+
+			String name, ip, mensaje;
+
+			EnvioCompleto paquete_recibido;
+			
+
+			while(true){
+
+				Socket misocket= servidor.accept();
+				ObjectInputStream paquete_datos= new ObjectInputStream(misocket.getInputStream());
+
+				paquete_recibido= (EnvioCompleto) paquete_datos.readObject();
+
+				name= paquete_recibido.getName();
+				ip= paquete_recibido.getIp();
+				mensaje= paquete_recibido.getMensaje();
+
+
+
+				/*DataInputStream flujo_entrada= new DataInputStream(misocket.getInputStream());
+
+				String mensaje_texto=flujo_entrada.readUTF();
+
+				areatexto.append("\n" + mensaje_texto);*/
+				areatexto.append("\n"+ name+ ": " + mensaje + "Enviado a: " +ip);
+
+				misocket.close();
+
+			}
+
+		} catch (IOException | ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
 	private	JTextArea areatexto;
 }
